7c2d4b7c3f860b1d7f0ede4bd0b79a3099230d6e
John Seby <johnseby253@gmail.com>
2025-12-07 12:09:13 +0530

    remove baseline map

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,6 +39,11 @@ export default function RootLayout({ children }) {
         <Footer/>
 
 
+  <link rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" 
+          integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+          crossOrigin="anonymous" 
+          referrerPolicy="no-referrer" />
 
 
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
